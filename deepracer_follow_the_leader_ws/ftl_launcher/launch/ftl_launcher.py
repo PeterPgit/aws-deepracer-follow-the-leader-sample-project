0fac9b9419cd447a05784bcb8dc42fddf9b7a768
Peter <github@loginp.me>
2025-03-12 12:44:47 -0500

    Update ftl_launcher.py

--- deepracer_follow_the_leader_ws/ftl_launcher/launch/ftl_launcher.py
+++ deepracer_follow_the_leader_ws/ftl_launcher/launch/ftl_launcher.py
@@ -26,7 +26,7 @@ def generate_launch_description():
         executable='object_detection_node',
         name='object_detection_node',
         parameters=[{
-            'DEVICE': 'CPU',
+            'DEVICE': 'MYRIAD',
             'PUBLISH_DISPLAY_OUTPUT': True
         }]
         )
